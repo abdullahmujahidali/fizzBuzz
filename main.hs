1bede4f0e1fd99b85e536ec5092e99fb4bf2dbe6
Abdullah Mujahid <abdullahmujahidali1@gmail.com>
2020-10-18 14:25:10 +0000

    Initial commit

---
+++ main.hs
@@ -0,0 +1,8 @@
+fizz :: Int -> String
+fizz n | n `mod` 15 == 0  = "FizzBuzz"
+       | n `mod` 3  == 0  = "Fizz"
+       | n `mod` 5  == 0  = "Buzz"
+       | otherwise = show n
+
+main :: IO()
+main = mapM_ putStrLn $ map fizz [1..100]
